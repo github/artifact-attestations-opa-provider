321fce2463e874e45e96479245a199b6f1dc8d30
Fredrik Skogman <kommendorkapten@github.com>
2025-04-24 08:57:53 +0200

    forgot to commit the metrics package

---
+++ pkg/metrics/prom.go
@@ -0,0 +1,38 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	AttestationsRetrieved = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "aaop_attestations_retrieved_total",
+		Help: "The total number of attestations retrieved",
+	})
+
+	AttestationsVerOk = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "aaop_attestations_verified_ok",
+		Help: "The total number of attestations verified",
+	})
+
+	AttestationsVerFail = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "aaop_attestations_verified_fail",
+		Help: "The total number of attestations that failed verification",
+	})
+
+	AttestationsPullTimer = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "aaop_attestations_retrieved_timer",
+		Help: "The duration (seconds) for fetching attestations from the OCI registry",
+	})
+
+	AttestationsVerTimer = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "aaop_attestations_verifcation_timer",
+		Help: "The duration (seconds) for verifying attestations",
+	})
+
+	AttestationsReqTimer = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "aaop_attestations_request_timer",
+		Help: "The duration (seconds) for the entire request processing",
+	})
+)
