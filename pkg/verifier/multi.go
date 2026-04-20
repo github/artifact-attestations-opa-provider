9adc50d85b1800df75e76ef45a043ab2e9e0bb00
Fredrik Skogman <kommendorkapten@github.com>
2026-04-20 16:03:42 +0200

    spelling fixes

--- pkg/verifier/multi.go
+++ pkg/verifier/multi.go
@@ -36,7 +36,7 @@ func NewMulti(v []*Verifier) *Multi {
 }
 
 // Verify iterates over each bundle, and verifies the bundle against
-// all known trust roots. If a sucessful verification occurs, no other
+// all known trust roots. If a successful verification occurs, no other
 // trust roots are tried.
 func (m *Multi) Verify(bundles []*bundle.Bundle, h *v1.Hash) ([]*verify.VerificationResult, error) {
 	var res = []*verify.VerificationResult{}
